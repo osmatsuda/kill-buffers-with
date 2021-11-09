62ad4608ac50110d3c243427e7e7a2c7720527ef
osmatsuda <osmatsuda@gmail.com>
2021-11-09 11:38:42 +0900

    add README contents

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # kill-buffers-with
+
+## Usage
+
+Allows killing some buffers at one time like `ag-kill-buffers` or `tramp-cleanup-all-buffers`, but you can select buffers with a glob pattern and a search object.
+
+A glob pattern is translated to a regexp and search for matched buffers from `(buffer-list)`. The following terms you can choose for a search object type:
+
+* `buffer`
+   * `(buffer-name BUFFER)`
+* `file`
+   * `(buffer-file-name BUFFER)`
+   * or `(buffer-local-value 'list-buffers-directory BUFFER)`
+* `mode`
+   * `(buffer-local-value 'mode-name BUFFER)`
+
+### Example
+
+When you want to kill all buffers under a directry not only file-buffers but like dired and magit buffers.
+
+```
+---------- Buffer: Minibuffer ----------
+M-x kill-buffers-with <RET>
+
+Pattern: */path-to-kill/* <RET>
+
+Select name type: file <RET>
+---------- Buffer: Minibuffer ----------
+
+---------- Echo Area ----------
+Killed buffers ("__init__.py<foo>" "__init__.py<bar>" "path-to-kill" "magit: path-to-kill" "magit-process: kill-to-kill")
+---------- Echo Area ----------
+```
+
+See [test.el](test.el) how a glob pattern translate to a regexp.
+
+## Installation
+
+Put [kill-buffers-with.el](kill-buffers-with.el) in your `load-path` directory and execute `(autoload 'kill-buffers-with "kill-buffers-with" nil t)`.
+
+
--- kill-buffers-with.el
+++ kill-buffers-with.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021  osmatsuda
 
 ;; Author: osmatsuda;;  <osmatsuda@gmail.com>
-;; Keywords: 
+;; Keywords: convenience
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -27,44 +27,45 @@
 (require 'cl-lib)
 
 (defun pattern2regexp (pat)
+  ""
   (cl-labels
       ((rec (pat-chars result)
-	    (pcase pat-chars
-	      ((pred null) (reverse result))
-	      (`(?* . ,tail) (rec tail (cons '(* not-newline)
-					     result)))
-	      (`(?? . ,tail) (rec tail (cons 'not-newline
-					     result)))
-	      ((and `(?\\ . ,tail)
-		    (guard (not (null tail))))
-	       (rec (cdr tail) (cons (char-to-string (car tail))
-				     result)))
-	      ((and `(?\[ . ,tail)
-		    (app (member ?\]) rest)
-		    (guard (not (null rest))))
-	       (rec (cdr rest)
-		    (cons (list 'in (concat
-				     (cl-loop for c in tail
-					      until (= c ?\])
-					      collect c)))
-			  result)))
-	      ((and `(?\{ . ,tail)
-		    (app (member ?\}) rest)
-		    (guard (not (null rest))))
-	       (rec (cdr rest)
-		    (cons (cons 'or
-				(split-string (concat
-					       (cl-loop for c in tail
-							until (= c ?\})
-							if (not (= c ? )) collect c)) ","))
-			  result)))
-	      (`(,c . ,tail)
-	       (rec tail (cons (char-to-string c)
-			       result)))))
+            (pcase pat-chars
+              ((pred null) (reverse result))
+              (`(?* . ,tail) (rec tail (cons '(* not-newline)
+                                             result)))
+              (`(?? . ,tail) (rec tail (cons 'not-newline
+                                             result)))
+              ((and `(?\\ . ,tail)
+                    (guard (not (null tail))))
+               (rec (cdr tail) (cons (char-to-string (car tail))
+                                     result)))
+              ((and `(?\[ . ,tail)
+                    (app (member ?\]) rest)
+                    (guard (not (null rest))))
+               (rec (cdr rest)
+                    (cons (list 'in (concat
+                                     (cl-loop for c in tail
+                                              until (= c ?\])
+                                              collect c)))
+                          result)))
+              ((and `(?\{ . ,tail)
+                    (app (member ?\}) rest)
+                    (guard (not (null rest))))
+               (rec (cdr rest)
+                    (cons (cons 'or
+                                (split-string (concat
+                                               (cl-loop for c in tail
+                                                        until (= c ?\})
+                                                        if (not (= c ? )) collect c)) ","))
+                          result)))
+              (`(,c . ,tail)
+               (rec tail (cons (char-to-string c)
+                               result)))))
        (exec-rx (pseq)
-		(eval `(rx (seq line-start
-				,@pseq
-				line-end)))))
+                (eval `(rx (seq line-start
+                                ,@pseq
+                                line-end)))))
     (exec-rx
      (rec (string-to-list pat) nil))))
 
@@ -72,37 +73,37 @@
   (cond
    ((string= "buffer" name)
     #'(lambda (regexp buffer)
-	(string-match regexp (buffer-name buffer))))
+        (string-match regexp (buffer-name buffer))))
    ((string= "mode" name)
     #'(lambda (regexp buffer)
-	(let ((mode-name (buffer-local-value 'mode-name buffer)))
-	  (unless (stringp mode-name) (setq mode-name ""))
-	  (string-match regexp mode-name))))
+        (let ((mode-name (buffer-local-value 'mode-name buffer)))
+          (unless (stringp mode-name) (setq mode-name ""))
+          (string-match regexp mode-name))))
    ((string= "file" name)
     #'(lambda (regexp buffer)
-	(string-match regexp
-		      (abbreviate-file-name
-		       (or (buffer-file-name buffer)
-			   (buffer-local-value 'list-buffers-directory buffer)
-			   "")))))
+        (string-match regexp
+                      (abbreviate-file-name
+                       (or (buffer-file-name buffer)
+                           (buffer-local-value 'list-buffers-directory buffer)
+                           "")))))
    (t (error ""))))
 
 (defun kill-buffers-with (pat type)
   (interactive (list (read-string "Pattern: ")
-		     (completing-read "Select name type: "
-				      '("buffer" "file" "mode")
-				      nil t)))
+                     (completing-read "Select name type: "
+                                      '("buffer" "file" "mode")
+                                      nil t)))
   (let ((regexp (pattern2regexp pat))
-	(match (kill-buffers-with--type type)))
+        (match (kill-buffers-with--type type)))
     (loop for b in (buffer-list)
-	  for bn = (buffer-name b)
-	  with targets = nil
-	  when (and (not (string= " " (substring bn 0 1)))
-		    (funcall match regexp b))
-	  collect bn into targets
-	  and do (kill-buffer b)
-	  finally (when targets
-		    (message "killed buffers %S" targets)))))
+          for bn = (buffer-name b)
+          with targets = nil
+          when (and (not (string= " " (substring bn 0 1)))
+                    (funcall match regexp b))
+          collect bn into targets
+          and do (kill-buffer b)
+          finally (when targets
+                    (message "Killed buffers %S" targets)))))
 
 (provide 'kill-buffers-with)
 ;;; kill-buffers-with.el ends here
